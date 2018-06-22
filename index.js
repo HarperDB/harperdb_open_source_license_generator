8da7f0a0a7a64a9e2694e088c4c5c031c2cd0e62
kyle <kyle@harperdb.io>
2018-06-22 13:48:27 -0600

    updated to handle a bug when a library begins with @

--- index.js
+++ index.js
@@ -6,31 +6,42 @@ const legal = require('legal-eagle'),
 
 //used to generate each package attribution format
 const template = fs.readFileSync('./template.txt').toString();
-
-legal({path:config.harperdb_project_path}, (err, summary)=>{
+let summary = null;
+legal({path:config.harperdb_project_path}, (err, summ)=>{
     let master_licence_text = '';
 
     if(err){
         console.error(err);
+    } else{
+        summary = summ;
     }
 
     let unique_summary = {};
     Object.keys(summary).forEach((dependency_key)=>{
         let split_dependency = dependency_key.split('@');
+
+        //if the dependency name begins with @ the first element of our array ends up incorrectly being empty, so we remove it
+        if(split_dependency[0] == ""){
+            split_dependency.shift()
+        }
         let dependency_name = split_dependency[0];
         let dependency_version = split_dependency[1];
         let dependency_object = summary[dependency_key];
 
         //creating a unique listing for each dependency, there are occaisions where there are multiple versions of a package
         //in thatt instance we compare the versions and use the most recent
-        if(!unique_summary[dependency_name] || (unique_summary[dependency_name] && compare_versions(dependency_version, unique_summary[dependency_name].version) > 0)){
-            unique_summary[dependency_name] = {
-                dependency_name: dependency_name,
-                version: dependency_version,
-                repository: dependency_object.repository ? dependency_object.repository.replace('git+', '') : '',
-                license_type: dependency_object.license,
-                license_text: dependency_object.sourceText
-            };
+        try {
+            if (!unique_summary[dependency_name] || (unique_summary[dependency_name] && compare_versions(dependency_version, unique_summary[dependency_name].version) > 0)) {
+                unique_summary[dependency_name] = {
+                    dependency_name: dependency_name,
+                    version: dependency_version,
+                    repository: dependency_object.repository ? dependency_object.repository.replace('git+', '') : '',
+                    license_type: dependency_object.license,
+                    license_text: dependency_object.sourceText
+                };
+            }
+        } catch(e){
+            console.error(e);
         }
     });
 
